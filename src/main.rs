1e9150dcba3acfa2ac1242ce3354dad8936d94ef
hadish <moseshadish@gmail.com>
2025-12-15 13:00:19 +0200

    Fixed main to match setup result

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ mod app;
 mod helper_functions;
 
 use app::Mbash;
-use logger::{LogLevel, Logger, stdout_logger::StdoutLogger};
+use logger::{LogLevel, Logger, error, stdout_logger::StdoutLogger};
 
 fn main() {
     let logger: Box<dyn Logger> = Box::new(StdoutLogger::new(LogLevel::DEBUG));
     let mut mbash = Mbash::new(logger);
-    mbash.setup();
-
-    let _ = mbash.run();
+    match mbash.setup() {
+        Ok(_) => {
+            mbash.run();
+        },
+        Err(e) => eprintln!("Mbash setup process failed. Check logs for more details. '{}'.", e)
+    }
 }
