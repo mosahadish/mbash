69dc871adb67c0ed078b6418ec2329d34e20630f
hadish <moseshadish@gmail.com>
2025-12-16 08:54:13 +0200

    Store commands and their fn in a HashMap

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,9 @@
 use anyhow::{Context, Result, anyhow};
 use logger::{Logger, debug, error, info};
 use std::{
+    collections::HashMap,
     fs,
-    process::{Command, ExitCode, exit},
+    process::{Command, ExitCode},
 };
 
 use crate::helper_functions;
@@ -25,36 +26,47 @@ pub struct Mbash {
     tracking_files: Vec<String>,
     logger: Box<dyn Logger>,
     internal_command_prefix: &'static str,
-    exit_command: &'static str,
+    commands: HashMap<String, fn(&mut Mbash, &[&str])>,
 }
 
 impl Mbash {
     pub fn new(logger: Box<dyn Logger>) -> Self {
+        let mut command_map: HashMap<String, fn(&mut Self, &[&str])> = HashMap::new();
+
+        command_map.insert("ls".to_string(), list_files);
+        command_map.insert("cd".to_string(), cd);
+        command_map.insert("init".to_string(), init);
+        command_map.insert("exit".to_string(), exit);
+
         Mbash {
             exiting: Arc::new(AtomicBool::new(false)),
             current_path: PathBuf::new(),
             logger: logger,
             internal_command_prefix: "m",
             tracking_files: Vec::new(),
-            exit_command: "exit",
+            commands: command_map,
         }
     }
 
     pub fn setup(&mut self) -> Result<()> {
-        self.set_current_dir();
+        self.set_current_dir()
+            .context("Failed to setup mbash, failed to set current dir.")?;
         self.load_tracking_file()
             .context("Failed to setup mbash, failed to load tracking file.")?;
 
         Ok(())
     }
 
-    fn set_current_dir(&mut self) {
+    fn set_current_dir(&mut self) -> Result<()> {
         let current_dir_result = env::current_dir();
         match current_dir_result {
-            Ok(path) => self.current_path = path,
+            Ok(path) => {
+                self.current_path = path;
+                Ok(())
+            }
             Err(e) => {
                 error!(self.logger, "Failed to fetch current directory path. {}", e);
-                self.exit();
+                Err(e.into())
             }
         }
     }
@@ -110,126 +122,54 @@ impl Mbash {
         }
 
         let first_word = parts[0];
+        let mut command_name_index = 0;
         if first_word == self.internal_command_prefix {
-            debug!(self.logger, "Received an internal command.");
-            let command_name = parts[1];
-            let args = &parts[2..];
-
-            self.execute_internal_command(command_name, args);
-            return;
+            command_name_index += 1;
         }
 
-        let command_name = parts[0];
-        let args = &parts[1..];
+        let args_index = command_name_index + 1;
 
-        if command_name == self.exit_command {
-            self.exit();
-            info!(
-                self.logger,
-                "Received '{}' command, exiting mbash.", self.exit_command
-            );
+        let command_name = parts[command_name_index];
+        let args = &parts[args_index..];
 
-            self.exit();
-            return;
+        if self.commands.contains_key(command_name) {
+            self.commands[command_name](self, args);
         }
+        // if command_name == self.exit_command {
+        //     self.exit();
+        //     info!(
+        //         self.logger,
+        //         "Received '{}' command, exiting mbash.", self.exit_command
+        //     );
 
-        self.execute_external_command(command_name, args);
+        //     self.exit();
+        //     return;
+        // }
+
+        // self.execute_external_command(command_name, args);
     }
 
-    fn execute_internal_command(&mut self, command_name: &str, args: &[&str]) {
-        debug!(self.logger, "Received internal '{}' command.", command_name);
-        if command_name == "init" {
-            // TODO
-            _ = helper_functions::attempt_create_file(IGNORE_FILE_NAME);
-            _ = helper_functions::attempt_create_file(TRACKING_FILE_NAME);
-            return;
-        }
-        if command_name == "cd" {
-            self.cd(args);
-        }
-    }
+    // fn execute_internal_command(&mut self, command_name: &str, args: &[&str]) {
+    //     debug!(self.logger, "Received internal '{}' command.", command_name);
+    //     if command_name == "init" {
+    //         // TODO
+    //         _ = helper_functions::attempt_create_file(IGNORE_FILE_NAME);
+    //         _ = helper_functions::attempt_create_file(TRACKING_FILE_NAME);
+    //         return;
+    //     }
+    // }
 
-    fn execute_external_command(&mut self, command_name: &str, args: &[&str]) {
-        debug!(self.logger, "Received external '{}' command.", command_name);
+    // fn execute_external_command(&mut self, command_name: &str, args: &[&str]) {
+    //     debug!(self.logger, "Received external '{}' command.", command_name);
 
-        if command_name == "cd" {
-            self.cd(args);
-            return;
-        }
-        if command_name == "ls" {
-            self.list_files(args)
-        }
-    }
-
-    fn list_files(&mut self, args: &[&str]) {
-        match std::fs::read_dir(&self.current_path) {
-            Ok(entries) => {
-                for entry_result in entries {
-                    match entry_result {
-                        Ok(entry) => {
-                            let file_name = entry.file_name();
-                            let file_type_result = entry.file_type();
-                            match file_type_result {
-                                Ok(file_type) => {
-                                    let is_dir = file_type.is_dir();
-                                    if is_dir {
-                                        println!("{} [DIR]", file_name.to_string_lossy(),);
-                                    }
-                                }
-                                Err(e) => {
-                                    debug!(self.logger, "Failed determining whether '{}' is a dir or not due to an error '{}'", file_name.to_string_lossy(), e);
-                                    println!("{} [?]", file_name.to_string_lossy());
-                                }
-                            }
-                        }
-                        Err(e) => {
-                            error!(self.logger, "Error reading file entry: {}", e);
-                        }
-                    }
-                }
-            }
-            Err(e) => {
-                error!(
-                    self.logger,
-                    "Failed to read current directory's contents due to an error '{}'. 'ls' command failed.",
-                    e
-                );
-            }
-        }
-    }
-
-    fn cd(&mut self, args: &[&str]) {
-        debug!(self.logger, "huh");
-        let new_dir = &args[0];
-        if new_dir.is_empty() {
-            debug!(
-                self.logger,
-                "'cd' command requires a directory as an argument [cd <directory>]."
-            );
-            return;
-        }
-
-        match env::set_current_dir(new_dir) {
-            Ok(()) => {
-                debug!(self.logger, "Changed directory to '{}'.", new_dir);
-                self.set_current_dir();
-            }
-            Err(e) => {
-                error!(
-                    self.logger,
-                    "Failed to change directory to '{}': '{}'.", new_dir, e
-                );
-            }
-        }
-    }
-
-    pub fn exit(&self) {
-        if self.exiting.load(Ordering::Relaxed) {
-            return;
-        }
-
-        self.exiting.store(true, Ordering::Relaxed);
-    }
+    //     if command_name == "cd" {
+    //         self.cd(args);
+    //         return;
+    //     }
+    //     if command_name == "ls" {
+    //         list_files(self, args);
+    //     }
+    // }
 
     fn load_tracking_file(&mut self) -> io::Result<()> {
         let _ = helper_functions::attempt_create_file(TRACKING_FILE_NAME)?;
@@ -252,4 +192,84 @@ impl Mbash {
             Err(e) => Err(e),
         }
     }
+}
+
+fn list_files(mbash: &mut Mbash, args: &[&str]) {
+    match std::fs::read_dir(&mbash.current_path) {
+        Ok(entries) => {
+            for entry_result in entries {
+                match entry_result {
+                    Ok(entry) => {
+                        let file_name = entry.file_name();
+                        let file_type_result = entry.file_type();
+                        match file_type_result {
+                            Ok(file_type) => {
+                                let is_dir = file_type.is_dir();
+                                if is_dir {
+                                    println!("{} [DIR]", file_name.to_string_lossy(),);
+                                }
+                            }
+                            Err(e) => {
+                                debug!(
+                                    mbash.logger,
+                                    "Failed determining whether '{}' is a dir or not due to an error '{}'",
+                                    file_name.to_string_lossy(),
+                                    e
+                                );
+                                println!("{} [?]", file_name.to_string_lossy());
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        error!(mbash.logger, "Error reading file entry: {}", e);
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            error!(
+                mbash.logger,
+                "Failed to read current directory's contents due to an error '{}'. 'ls' command failed.",
+                e
+            );
+        }
+    }
+}
+
+fn cd(mbash: &mut Mbash, args: &[&str]) {
+    let new_dir = &args[0];
+    if new_dir.is_empty() {
+        debug!(
+            mbash.logger,
+            "'cd' command requires a directory as an argument [cd <directory>]."
+        );
+        return;
+    }
+
+    match env::set_current_dir(new_dir) {
+        Ok(()) => {
+            debug!(mbash.logger, "Changed directory to '{}'.", new_dir);
+            mbash.set_current_dir();
+        }
+        Err(e) => {
+            error!(
+                mbash.logger,
+                "Failed to change directory to '{}': '{}'.", new_dir, e
+            );
+        }
+    }
+}
+
+fn init(mbash: &mut Mbash, args: &[&str]) {
+    // TODO
+    _ = helper_functions::attempt_create_file(IGNORE_FILE_NAME);
+    _ = helper_functions::attempt_create_file(TRACKING_FILE_NAME);
+}
+
+fn exit(mbash: &mut Mbash, args: &[&str]) {
+    if mbash.exiting.load(Ordering::Relaxed) {
+        return;
+    }
+
+    mbash.exiting.store(true, Ordering::Relaxed);
 }
